1eda4a13bb110b2b932c07a01c6bc1761721d501
scchang <a0928136004@gmail.com>
2023-09-03 00:55:49 +0800

    modified

--- 小程式/crawler_stock/crawler_stock.py
+++ 小程式/crawler_stock/crawler_stock.py
@@ -5,11 +5,14 @@ alert_condtions = [
      'down_point':[10, True],
      'up_point':[10, True]},
     {'ch':'2891.tw',
-     'down_point':[0.01, True],
-     'up_point':[0.01, True]},     
+     'down_percent':[0.1, False],
+     'up_percent':[0.1, False]},
+    {'ch':'00878.tw',
+     'down_percent':[0.1, False],
+     'up_percent':[0.1, False]},       
     ]
 def download_stock():
-    url = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?json=1&delay=0&ex_ch=tse_t00.tw|tse_2891.tw"
+    url = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?json=1&delay=0&ex_ch=tse_t00.tw|tse_2891.tw|tse_00878.tw"
     response = requests.get(url)
     if response.status_code == 200:
         stock = response.json()
@@ -17,7 +20,7 @@ def download_stock():
     else:
         return False
 def check_stock_alert(stock):
-    print(stock)
+    #print(stock)
     for i in stock['msgArray']:
         if not ('y' in i and 'z' in i):
             print("less some stock keys")
@@ -31,16 +34,39 @@ def check_stock_alert(stock):
         z = float(i['z'])
         for j in alert_condtions:
             if ch == j['ch']:
-                if z <= y and y-z >= j['down_point'][0] and j['down_point'][1] == True:
-                    print(f"{n}: 目前 {z}, 大跌 {y-z}")
-                elif z >= y and z-y >= j['up_point'][0] and j['up_point'][1] == True:
-                    print(f"{n}：目前 {z}, 大漲 {z-y}")
+                if 'down_point' in j:
+                    val = abs(z-y)
+                    if z < y and val > j['down_point'][0] and j['down_point'][1] == True:
+                        print(f"{n}: 目前 {z} 點, 大跌 {round(val,2)} 點")
+                if 'up_point' in j:
+                    val = abs(z-y)
+                    if z > y and val > j['up_point'][0] and j['up_point'][1] == True:
+                        print(f"{n}: 目前 {z} 點, 大漲 {round(val,2)} 點")
+                if 'down_percent' in j:
+                    val = abs((y-z)/y*100)
+                    if z < y and val > j['down_percent'][0] and j['down_percent'][1] == True:
+                        print(f"{n}: 目前 {z} 點, 大跌 {round(val,2)} 趴")
+                if 'up_percent' in j:
+                    val = abs((y-z)/y*100)
+                    if z > y and val > j['up_percent'][0] and j['up_percent'][1] == True:
+                        print(f"{n}：目前 {z} 點, 大漲 {round(val,2)} 趴")
+
+def is_in_trade_datetime(stock:dict)->bool:
+    sys_date = stock['queryTime']['sysDate']
+    sys_time = stock['queryTime']['sysTime']
+    trade_date = stock['msgArray'][0]['d']
+    if sys_date == trade_date and sys_time >= "09:00:00" and sys_time <= "13:30:00":
+        print("在交易時間內")
+        return True
+    print("非交易時間內")
+    return False
 
 def dowload_stock_tmr():
     stock = download_stock()
     if stock!= False:
         print("下載成功")
-        check_stock_alert(stock)
+        if is_in_trade_datetime(stock) == True:
+            check_stock_alert(stock)
     else:
         print("下載失敗")
     t = threading.Timer(5, dowload_stock_tmr)
